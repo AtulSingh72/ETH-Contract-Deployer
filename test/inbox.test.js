f99faca74d664a0452eb8724700815d3582aa315
AtulSingh72 <atul.as72@gmail.com>
2021-10-18 14:54:31 +0530

    Test Functions Complete

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -20,4 +20,13 @@ describe('Inbox', () => {
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
     });
+    it('has a default message', async () => {
+        const message = await inbox.methods.getMessage().call();
+        assert.equal(message, "Hi There!")
+    });
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('New Message').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'New Message')
+    });
 }); 
